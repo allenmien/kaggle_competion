54bef70f1f20287cc5fd4ec04323a4a6e2a9bbf2
maqi <maqi@xiaoyouzi.com>
2019-02-13 19:15:22 +0800

    pca

---
+++ digit_recognizer/mark/__init__.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8-*-
+"""
+@Time   : 2019-02-13 18:26
+@Author : Mark
+@File   : __init__.py.py
+"""
--- digit_recognizer/mark/svm_classifier.py
+++ digit_recognizer/mark/svm_classifier.py
@@ -2,7 +2,7 @@
 """
 @Time   : 2019-02-13 11:08
 @Author : Mark
-@File   : svm_mark.py
+@File   : svm_classifier.py
 
 Kaggle Data: https://www.kaggle.com/c/digit-recognizer/data
 """
@@ -10,25 +10,33 @@ Kaggle Data: https://www.kaggle.com/c/digit-recognizer/data
 print(__doc__)
 import os
 
+import numpy as np
 import pandas as pd
 from sklearn import svm
+from sklearn.decomposition import pca
 
 file_path = "./data/svm_out.csv"
 train_pd = pd.read_csv('./data/train.csv')
 test_pd = pd.read_csv('./data/test.csv')
 
-train_X_pd = train_pd.iloc[:50, 1:]
-train_Y_pd = train_pd.iloc[:50, :1]
-test_X_pd = test_pd.iloc[:50, :]
+train_X_pd = train_pd.iloc[:, 1:]
+train_Y_pd = train_pd.iloc[:, :1]
+test_X_pd = test_pd.iloc[:, :]
 
 train_X = train_X_pd.values
 train_Y = train_Y_pd.values
 test_X = test_X_pd.values
 
-clf = svm.SVC()
-clf.fit(train_X, train_Y)
+pca = pca.PCA(n_components=0.8, whiten=True)
+train_X = pca.fit_transform(train_X)
+test_X = pca.fit_transform(test_X)
+print(pca.explained_variance_ratio_)
+print(np.sum(pca.explained_variance_ratio_))
 
-test_y_predict = clf.predict(test_X)
+model = svm.SVC(kernel='rbf', C=10)
+model.fit(train_X, train_Y)
+
+test_y_predict = model.predict(test_X)
 
 predict_pd = pd.DataFrame({'ImageId': range(1, len(test_X) + 1), 'Label': test_y_predict})
 
